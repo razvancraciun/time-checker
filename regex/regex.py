b29679f33eb4ac895c3ca45ef1a6a12ddcf6cebe
czrptr <cezar.c.petriuc@gmail.com>
2020-03-24 14:31:32 +0200

    regex api update

--- regex/regex.py
+++ regex/regex.py
@@ -7,12 +7,15 @@ res = Res()
 
 def redef(name, content):
 	global res
-	setattr(res, name, f"(?P<{name}>{content})")
+	if name[0:2] == "c_":
+		setattr(res, name[2:], f"({content})")
+	else:
+		setattr(res, name, f"(?P<{name}>{content})")
 
 redef("c_ore_nr", r"0?[1-9]|1[0-9]|2[0-3]")
 redef("c_minute_nr", r"0?[1-9]|[1-5][0-9]")
 redef("c_secunde_nr", r"0?[1-9]|[1-5][0-9]")
-redef("oră_nr", f"{res.c_ore_nr}:{res.c_minute_nr}:{res.c_secunde_nr}")
+redef("oră_nr", f"{res.ore_nr}:{res.minute_nr}:{res.secunde_nr}")
 redef("c_expresie_temporală", f"{res.oră_nr}")
 
 if __name__ == "__main__":
@@ -29,6 +32,6 @@ if __name__ == "__main__":
 		print(f'> {e.args[1]} "{file_path}"')
 		exit(1)
 
-	res = re.search(res.c_expresie_temporală, input_text, flags = re.I).groupdict()
-	res = {k : v for k, v in res.items() if k[0:2] != "c_"}
-	print(res)
+	res = re.search(res.expresie_temporală, input_text, flags = re.I).groupdict()
+	for k, v in res.items():
+		print(f'{k}: "{v}"')
