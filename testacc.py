77c4fd9b60e842c7a4604fe7a512fda83c6c93e2
Razvan Ioan Craciun <razvan.craciun@softvision.ro>
2020-05-14 13:54:35 +0300

    37.4 accuracy

--- testacc.py
+++ testacc.py
@@ -2,6 +2,8 @@ from ml.preprocess_acc import COMBS
 from ml.bayes import BayesClassifier, load_pickle
 import os
 from nltk.tokenize import RegexpTokenizer
+from regex.redefs import timex_matched
+from tqdm import tqdm
 
 
 TEST_PATH = os.path.dirname(os.path.realpath(__file__)) + '/ml/data/test/'
@@ -11,15 +13,14 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 total = 0
 found = 0
-for comb in COMBS:
+for comb in tqdm(COMBS):
     bayes = BayesClassifier(time_set=load_pickle(TEST_PATH + f'times{comb}.pkl'), other_set=load_pickle(TEST_PATH + f'other{comb}.pkl'))
     with open(RAW_PATH + f'{comb[0]}times_art{comb[1:]}.txt') as f:
         text = f.read()
         total += len(text)
         result = bayes.run_on_text(text)
-        print(result)
         for b, el in result:
             if b:
-                found += len(el)
-    exit()
+                found += sum([len(expr) for expr in timex_matched(el)])
 print(found / total)
+# 0.3748708186021213
