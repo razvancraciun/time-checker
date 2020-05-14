376f6798b59aa221bce4fea7a03291b45a64de33
czrptr <cezar.c.petriuc@gmail.com>
2020-05-14 12:13:44 +0300

    fixed bug

--- ml/bayes.py
+++ ml/bayes.py
@@ -28,55 +28,24 @@ class BayesClassifier:
         tokens = list(custom_tokenize(text))
         for (is_words, expr) in tokens:
             if is_words:
-                tokens = re.split('\s+', expr)
-                is_temporal_expr = self.classify(stem(tokens)[0])
-                result += [(is_temporal_expr, ' '.join(tokens))]
+                tokens = re.split(r'\s+', expr)
+                is_temp_exprs = [self.classify(word) for word in stem(tokens)]
+                for ii in range(len(tokens)):
+                    result += [(is_temp_exprs[ii], tokens[ii])]
             else:
-                result += [(False, expr)]
-        return result
+                result[-1] = (result[-1][0], result[-1][1] + expr)
 
+        final = [result[0]]
 
-    # def run_on_text(self, text):
-    #     result = []
-    #     tokens = list(custom_tokenize(text))
-    #     for (is_word, token) in tokens:
-    #         if is_word:
-    #             is_temporal_expr = self.classify(stem([token])[0])
-    #             result += [(is_temporal_expr, token)]
-    #         else:
-    #             result += [(False, token)]
-    #     return result
+        for ii in range(1, len(result)):
+            if final[-1][0] != result[ii][0]:
+                final += [result[ii]]
+            else:
+                space = '' if final[-1][1].endswith('-') or final[-1][1].endswith(' ') else ' '
+                final[-1] = (final[-1][0], final[-1][1] + space + result[ii][1])
 
+        return final
 
-    # def run_on_text(self, text):
-    #     tokenizer = RegexpTokenizer(r'\w+')
-    #     words = tokenizer.tokenize(text.lower())
-    #     return self.run(words)
-
-    # ''' Text should be a list of words (eg. ['după', 'câteva', 'luni']) without any spaces or punctuation. Use misc_utils.preprocess to preprocess the text'''
-    # def run(self, words):
-    #     if len(words) == 0:
-    #         return []
-
-    #     stemmed_words = stem(words)
-    #     bool_mask = [self.classify(word) for word in (stemmed_words)]
-        
-    # 	# self.classify(stem([word])[0])
-
-    #     result = []
-    #     partial = ''
-    #     last_bool = bool_mask[0]
-    #     for i, el in enumerate(bool_mask):
-    #         if el == last_bool:
-    #             partial += ' ' + words[i]
-    #         else:
-    #             result.append( (last_bool, partial) ) 
-    #             last_bool = el
-    #             partial = words[i] 
-
-    #     result.append( (last_bool, partial) )
-    #     return result
-    
 
     ''' True if word is part of timex, False otherwids '''
     def classify(self, word):
